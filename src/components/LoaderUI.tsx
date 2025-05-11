f247d11d9ffd88c82a05af45688d942b04f5c314
Paras <mehtaparas1901@gmail.com>
2025-05-11 12:16:39 +0530

    Loader component implemented

---
+++ src/components/LoaderUI.tsx
@@ -0,0 +1,11 @@
+import {HashLoader} from "react-spinners"
+
+const LoaderUI = () => {
+  return (
+    <div className='flex items-center justify-center mt-14'>
+      <HashLoader color="white" size={100} />
+    </div>
+  )
+}
+
+export default LoaderUI
